0e2f45a16884ecb314be6500b2d8c08e48340306
lbrayner <lbrayner@users.noreply.github.com>
2017-12-18 16:56:41 -0300

    20171218165641

--- init.el
+++ init.el
@@ -35,6 +35,29 @@
  ;; If there is more than one, they won't work right.
  )
 
+; https://stackoverflow.com/a/37132338/2856535
+(defun my-org-inline-css-hook (exporter)
+  "Insert custom inline css"
+  (when (eq exporter 'html)
+    (let* ((dir (ignore-errors (file-name-directory (buffer-file-name))))
+           (path (concat dir "style.css"))
+           (homestyle (or (null dir) (null (file-exists-p path))))
+           (final (if homestyle "~/.emacs.d/org-style.css" path))) ;; <- set your own style file path
+      (setq org-html-head-include-default-style nil)
+      (setq org-html-head (concat
+                           "<style type=\"text/css\">\n"
+                           "<!--/*--><![CDATA[/*><!--*/\n"
+                           (with-temp-buffer
+                             (insert-file-contents final)
+                             (buffer-string))
+                           "/*]]>*/-->\n"
+                           "</style>\n")))))
+
+(defun lbrayner-org-mode-hook ()
+      (visual-line-mode)
+      (org-indent-mode)
+      (setq truncate-lines nil))
+
 ; major modes
     ; org
 (setq org-export-time-stamp-file nil)
@@ -43,6 +66,9 @@
 (setq-default org-export-with-toc nil)
 (setq-default org-use-sub-superscripts '{})
 (setq-default org-export-with-sub-superscripts '{})
+(add-hook 'org-mode-hook 'lbrayner-org-mode-hook)
+(add-hook 'org-export-before-processing-hook 'my-org-inline-css-hook)
+
 
 ; minor-modes
     ; built-in
@@ -123,6 +149,8 @@
             ; built-in
                 ; linum
 (set-face-attribute 'linum nil :height 100)
+                ; visual-line-mode
+(setq global-visual-line-mode t)
 
 ; custom functions and respective mappings
 
@@ -137,23 +165,3 @@
       (kill-new filename)
       (message "Copied buffer file name '%s' to the clipboard." filename))))
 
-
-; https://stackoverflow.com/a/37132338/2856535
-(defun my-org-inline-css-hook (exporter)
-  "Insert custom inline css"
-  (when (eq exporter 'html)
-    (let* ((dir (ignore-errors (file-name-directory (buffer-file-name))))
-           (path (concat dir "style.css"))
-           (homestyle (or (null dir) (null (file-exists-p path))))
-           (final (if homestyle "~/.emacs.d/org-style.css" path))) ;; <- set your own style file path
-      (setq org-html-head-include-default-style nil)
-      (setq org-html-head (concat
-                           "<style type=\"text/css\">\n"
-                           "<!--/*--><![CDATA[/*><!--*/\n"
-                           (with-temp-buffer
-                             (insert-file-contents final)
-                             (buffer-string))
-                           "/*]]>*/-->\n"
-                           "</style>\n")))))
-
-(add-hook 'org-export-before-processing-hook 'my-org-inline-css-hook)
