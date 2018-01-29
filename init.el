33dbf0d84872acfbd6c8f235e88f37bd6ee12e7e
lbrayner <lbrayner@users.noreply.github.com>
2018-01-29 10:13:09 -0300

    slime & new evil mappings && paren-mode
    Enabled paren-mode globally.
    
    Configured it for SBCL.
    
    Added evil mappings to switch between windows.
    
    In short, synced with windows_nt.

--- init.el
+++ init.el
@@ -87,8 +87,13 @@
 ; minor-modes
     ; built-in
         ; linum
+	; show-paren-mode
+(show-paren-mode 1)
 (global-linum-mode 1)
     ; melpa
+	; slime
+(setq inferior-lisp-program "sbcl")
+(setq slime-contribs '(slime-fancy))
         ; evil
 (evil-mode t)
 (global-evil-leader-mode)
@@ -102,6 +107,11 @@
 (define-key evil-insert-state-map "\C-u" '(lambda () (interactive) (kill-line 0)))
 (define-key evil-insert-state-map (kbd "<f6>") '(lambda () (interactive)
                                                   (evil-normal-state) (save-buffer)))
+(define-key evil-motion-state-map "\C-h" 'evil-window-left)
+(define-key evil-motion-state-map "\C-j" 'evil-window-down)
+(define-key evil-motion-state-map "\C-k" 'evil-window-up)
+(define-key evil-motion-state-map "\C-l" 'evil-window-right)
+
 ;; Bailey Ling
 (setq evil-emacs-state-cursor '("red" box))
 (setq evil-motion-state-cursor '("orange" box))
@@ -205,4 +215,3 @@
     (when filename
       (kill-new filename)
       (message "Copied buffer file name '%s' to the clipboard." filename))))
-
