05dcd35e36d573745fe7a64b2bbf03c1f9292450
lbrayner <lbrayner@users.noreply.github.com>
2018-06-09 05:24:43 -0300

    Added config-ox-pandoc.el

---
+++ config/config-ox-pandoc.el
@@ -0,0 +1,31 @@
+(defvar my-org-pandoc-temporary-css-file nil)
+
+(defun my-org-pandoc-inline-css-hook (exporter)
+  "Insert custom inline css (pandoc)"
+  (when (eq exporter 'pandoc)
+    (let* ((dir (ignore-errors (file-name-directory (buffer-file-name))))
+           (filename (concat (file-name-base (buffer-file-name)) ".css"))
+           (path (concat dir filename))
+           (default (or (null dir) (null (file-exists-p path))))
+           (default-file (expand-file-name ".emacs.d/org-style.css" (home-directory)))
+           (final (if default "~/.emacs.d/org-style.css" path)))
+      (if (file-exists-p final)
+	  (setf (alist-get 'include-before-body org-pandoc-options)
+		(let ((temp-file (make-temp-name "pandoc-css.")))
+		  (setq my-org-pandoc-temporary-css-file temp-file)
+		  (with-temp-file temp-file
+		    (insert (concat "<style type=\"text/css\">\n"
+				    "<!--/*--><![CDATA[/*><!--*/\n"))
+		    (save-excursion (insert (concat "/*]]>*/-->\n"
+						    "</style>\n")))
+		    (insert-file-contents final))
+		  temp-file))))))
+
+(defun my-org-pandoc-delete-temporary-css-file-hook ()
+  "Deletes the temporary css file."
+  (delete-file my-org-pandoc-temporary-css-file)
+  (setq my-org-pandoc-temporary-css-file nil))
+
+(add-hook 'org-export-before-processing-hook 'my-org-pandoc-inline-css-hook)
+(add-hook 'org-pandoc-after-processing-html5-pdf-hook 'my-org-pandoc-delete-temporary-css-file-hook)
+(add-hook 'org-pandoc-after-processing-html5-hook 'my-org-pandoc-delete-temporary-css-file-hook)
