791e03f54eec7a31886c56b388a1220011add7b2
lbrayner <lbrayner@users.noreply.github.com>
2018-07-10 23:36:46 -0300

    Redefined icicle-select-window-by-name for <f5>
    Redefined icicle-select-window-by-name (my-icicle-select-window) and
    used it for <f5> binding instead of icicle-select-window.

--- config/config-icicles.el
+++ config/config-icicles.el
@@ -1,9 +1,11 @@
 (require 'icicles)
 
-(defun my-icicle-select-window ()
-  "Calls `icicle-select-window' with `current-prefix-arg' set to \\='-."
-  (interactive)
-  (let ((current-prefix-arg  '-))
-    (icicle-select-window)))
+(defun my-icicle-select-window (win-name &optional window-alist)
+  "Calls `icicle-read-choose-window-args' with `current-prefix-arg'
+set to \\='- and then selects window via
+`icicle-choose-window-by-name'."
+  (interactive (let ((current-prefix-arg  '-))
+                 (icicle-read-choose-window-args)))
+  (icicle-choose-window-by-name win-name window-alist))
 
 (global-set-key (kbd "<f5>") 'my-icicle-select-window)
