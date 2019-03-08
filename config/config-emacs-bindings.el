13600b74ed018dd12047c0d755d4bbe85e97a132
lbrayner <lbrayner@users.noreply.github.com>
2019-03-07 23:53:24 -0300

    Move point to top or bottom of screen

--- config/config-emacs-bindings.el
+++ config/config-emacs-bindings.el
@@ -14,3 +14,17 @@
 (global-set-key (kbd "ESC <up>") #'scroll-down-line) 
 (global-set-key (kbd "<M-down>") #'scroll-up-line)
 (global-set-key (kbd "<M-up>") #'scroll-down-line) 
+
+(defun my-move-to-window-line-top ()
+  "Position point at the top of the window."
+  (interactive)
+  (move-to-window-line 0))
+
+(defun my-move-to-window-line-bottom ()
+  "Position point at the bottom of the window."
+  (interactive)
+  (move-to-window-line -1))
+
+;; point at top or bottom of screen
+(global-set-key (kbd "<C-home>") #'my-move-to-window-line-top)
+(global-set-key (kbd "<C-end>") #'my-move-to-window-line-bottom)
