c05adde4ff3cfc4fc09f5d6f4e9966aa63f0c738
lbrayner <lbrayner@users.noreply.github.com>
2019-03-09 13:46:09 -0300

    Hiding and replacing modeline strings
    With clean-mode-line.
    
    https://www.masteringemacs.org/article/hiding-replacing-modeline-strings

--- config/config-mode-line.el
+++ config/config-mode-line.el
@@ -33,3 +33,60 @@
 (setf (get-line-mode-column-mode-format mode-line-position)
       `(:eval (concat ,line-mode-column-mode-original-format " "
                       (total-lines-as-string))))
+
+;; https://www.masteringemacs.org/article/hiding-replacing-modeline-strings
+
+;;; Hiding and replacing modeline strings with clean-mode-line
+;;; By Mickey Petersen
+
+(defvar mode-line-cleaner-alist
+  `((auto-complete-mode . " α")
+    (yas/minor-mode . " υ")
+    (paredit-mode . " π")
+    (eldoc-mode . "")
+    (abbrev-mode . "")
+    ;; Major modes
+    (lisp-interaction-mode . "λ")
+    (hi-lock-mode . "")
+    (python-mode . "Py")
+    (emacs-lisp-mode . "EL")
+    (nxhtml-mode . "nx"))
+  "Alist for `clean-mode-line'.
+
+When you add a new element to the alist, keep in mind that you
+must pass the correct minor/major mode symbol and a string you
+want to use in the modeline *in lieu of* the original.")
+
+
+(defun clean-mode-line ()
+  (interactive)
+  (loop for cleaner in mode-line-cleaner-alist
+        do (let* ((mode (car cleaner))
+                 (mode-str (cdr cleaner))
+                 (old-mode-str (cdr (assq mode minor-mode-alist))))
+             (when old-mode-str
+                 (setcar old-mode-str mode-str))
+               ;; major mode
+             (when (eq mode major-mode)
+               (setq mode-name mode-str)))))
+
+
+(add-hook 'after-change-major-mode-hook 'clean-mode-line)
+
+;; Fly-mode code commented
+
+;;; alias the new `flymake-report-status-slim' to
+;;; `flymake-report-status'
+;; (defalias 'flymake-report-status 'flymake-report-status-slim)
+;; (defun flymake-report-status-slim (e-w &optional status)
+;;   "Show \"slim\" flymake status in mode line."
+;;   (when e-w
+;;     (setq flymake-mode-line-e-w e-w))
+;;   (when status
+;;     (setq flymake-mode-line-status status))
+;;   (let* ((mode-line " Φ"))
+;;     (when (> (length flymake-mode-line-e-w) 0)
+;;       (setq mode-line (concat mode-line ":" flymake-mode-line-e-w)))
+;;     (setq mode-line (concat mode-line flymake-mode-line-status))
+;;     (setq flymake-mode-line mode-line)
+;;     (force-mode-line-update)))
