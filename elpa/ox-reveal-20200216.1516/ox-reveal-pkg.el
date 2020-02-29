37ad2a5e1136f5afc6e5b77d9b05d045f231d89f
lbrayner <lbrayner@users.noreply.github.com>
2020-02-29 20:11:13 -0300

    Upgraded packages

---
+++ elpa/ox-reveal-20200216.1516/ox-reveal-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ox-reveal" "20200216.1516" "reveal.js Presentation Back-End for Org Export Engine" '((org "8.3")) :commit "aafedfd8052d1ac4b0ddf946b3258a3b5ae5bbbe" :keywords '("outlines" "hypermedia" "slideshow" "presentation") :authors '(("Yujie Wen <yjwen.ty at gmail dot com>")) :maintainer '("Yujie Wen <yjwen.ty at gmail dot com>"))
