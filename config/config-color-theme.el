a630196338a66b4f3c0ef85a3c7b99e43898853c
lbrayner <lbrayner@users.noreply.github.com>
2020-03-02 10:35:57 -0300

    solarized-init-frame hook

--- config/config-color-theme.el
+++ config/config-color-theme.el
@@ -1,6 +1,15 @@
 ;; themes
     ;; emacs-color-theme-solarized
 (load-theme 'solarized t)
+
+(defun solarized-init-frame (frame)
+  (let ((mode (if (display-graphic-p frame) 'light 'dark)))
+    (set-frame-parameter frame 'background-mode mode)
+    (set-terminal-parameter frame 'background-mode mode))
+  (enable-theme 'solarized))
+
+(add-hook 'after-make-frame-functions #'solarized-init-frame)
+
     ;; melpa
         ;; solarized
 ;; (setq solarized-use-variable-pitch nil
@@ -12,9 +21,8 @@
 ;     (load-theme 'dracula t))
 
   ;; from solarized-theme's solarized-dark
-(let ((yellow    "#b58900"))
+(let ((yellow "#b58900"))
   (when my-display-line-numbers-should-be-enabled
     (set-face-attribute 'line-number-current-line nil
                         :weight 'bold
                         :foreground yellow)))
-
