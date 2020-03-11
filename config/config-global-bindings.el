b0a51844f6b56a2a1821e8da5bbe889b5083306d
lbrayner <lbrayner@users.noreply.github.com>
2020-03-11 15:12:13 -0300

    isearch is actually a built-in package

--- config/config-global-bindings.el
+++ config/config-global-bindings.el
@@ -61,23 +61,3 @@
          (dired-next-line 2))))
 
 (advice-add #'beginning-of-buffer :after #'my-beginning-of-buffer-dwim)
-
-;; https://www.emacswiki.org/emacs/SearchAtPoint
-(defun isearch-yank-regexp (regexp)
-  "Pull REGEXP into search regexp." 
-  (let ((isearch-regexp nil)) ;; Dynamic binding of global.
-    (isearch-yank-string regexp))
-  (if (not isearch-regexp)
-      (isearch-toggle-regexp))
-  (isearch-search-and-update))
-
-(defun isearch-yank-symbol ()
-  "Put symbol at current point into search string."
-  (interactive)
-  (let ((sym (find-tag-default)))
-    (if (null sym)
-        (message "No symbol at point")
-      (isearch-yank-regexp
-       (concat "\\_<" (regexp-quote sym) "\\_>")))))
-
-(define-key isearch-mode-map (kbd "M-s C-s") 'isearch-yank-symbol)
