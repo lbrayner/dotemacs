cee75ae868025f0af001032b1bc063533e3c000f
lbrayner <lbrayner@users.noreply.github.com>
2020-03-11 14:48:17 -0300

    ibuffer & isearch merged in single file

---
+++ config/config-built-in.el
@@ -0,0 +1,40 @@
+    ;; ISEARCH
+;; https://www.emacswiki.org/emacs/SearchAtPoint
+(defun isearch-yank-regexp (regexp)
+  "Pull REGEXP into search regexp." 
+  (let ((isearch-regexp nil)) ;; Dynamic binding of global.
+    (isearch-yank-string regexp))
+  (if (not isearch-regexp)
+      (isearch-toggle-regexp))
+  (isearch-search-and-update))
+
+(defun isearch-yank-symbol ()
+  "Put symbol at current point into search string."
+  (interactive)
+  (let ((sym (find-tag-default)))
+    (if (null sym)
+        (message "No symbol at point")
+      (isearch-yank-regexp
+       (concat "\\_<" (regexp-quote sym) "\\_>")))))
+
+(define-key isearch-mode-map (kbd "M-s C-s") 'isearch-yank-symbol)
+
+    ;; IBUFFER
+;; https://www.emacswiki.org/emacs/IbufferMode#toc11
+;; https://github.com/purcell/emacs.d/blob/master/lisp/init-ibuffer.el
+(with-eval-after-load 'ibuffer
+  ;; Use human readable Size column instead of original one
+  (define-ibuffer-column size-h
+    (:name "Size" :inline t)
+    (file-size-human-readable (buffer-size))))
+
+;; Modify the default ibuffer-formats
+(setq ibuffer-formats
+      '((mark modified read-only " "
+              (name 18 18 :left :elide)
+              " "
+              (size-h 9 -1 :right)
+              " "
+              (mode 16 16 :left :elide)
+              " "
+              filename-and-process)))
