e877dd8d598ea553236a40089185b6ff71490d07
lbrayner <lbrayner@users.noreply.github.com>
2020-12-24 18:49:33 -0300

    Upgraded packages

--- elpa/auto-dim-other-buffers-20200704.122/auto-dim-other-buffers-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "auto-dim-other-buffers" "20200704.122" "Makes windows without focus less prominent" 'nil :commit "82626612bd1c4c5f36d110f6bff3f93c33b4149e" :authors '(("Michal Nazarewicz" . "mina86@mina86.com")) :maintainer '("Michal Nazarewicz" . "mina86@mina86.com") :url "https://github.com/mina86/auto-dim-other-buffers.el")
