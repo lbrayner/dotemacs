51407d17525b10afa9bc83af7307f225904ac794
lbrayner <lbrayner@users.noreply.github.com>
2021-02-27 01:52:49 -0300

    Simpler mode-line with faces

--- config/config-mode-line.el
+++ config/config-mode-line.el
@@ -56,45 +56,129 @@
 ;;; Hiding and replacing modeline strings with clean-mode-line
 ;;; By Mickey Petersen
 
-(defvar mode-line-cleaner-alist
-  `((auto-complete-mode . " α")
-    (yas/minor-mode . " υ")
-    (smartparens-mode . " π")
-    (evil-smartparens-mode . "")
-    (eldoc-mode . "")
-    (slime-autodoc-mode . "")
-    (abbrev-mode . "")
-    (undo-tree-mode . "")
-    (auto-dim-other-buffers-mode . "")
-    (smart-tab-mode . " ➤")
-    (visual-line-mode . " ←")
-    (flyspell-mode . "")
-    ;; Major modes
-    (fundamental-mode . "Φ")
-    (lisp-interaction-mode . "λ")
-    (hi-lock-mode . "")
-    (python-mode . "Py")
-    (emacs-lisp-mode . "Ɛ")
-    (lisp-mode . "£")
-    (org-mode . "Ω")
-    (nxhtml-mode . "nx"))
-  "Alist for `clean-mode-line'.
+;; (defvar mode-line-cleaner-alist
+;;   '((auto-complete-mode . " α")
+;;     (yas/minor-mode . " υ")
+;;     (smartparens-mode . " π")
+;;     (evil-smartparens-mode . "")
+;;     (eldoc-mode . "")
+;;     (slime-autodoc-mode . "")
+;;     (abbrev-mode . "")
+;;     (undo-tree-mode . "")
+;;     (auto-dim-other-buffers-mode . "")
+;;     (smart-tab-mode . " ➤")
+;;     (visual-line-mode . " ←")
+;;     (flyspell-mode . "")
+;;     ;; Major modes
+;;     (fundamental-mode . "Φ")
+;;     (lisp-interaction-mode . "λ")
+;;     (hi-lock-mode . "")
+;;     (python-mode . "Py")
+;;     (emacs-lisp-mode . "Ɛ")
+;;     (lisp-mode . "£")
+;;     (org-mode . "Ω")
+;;     (nxhtml-mode . "nx"))
+;;   "Alist for `clean-mode-line'.
 
-When you add a new element to the alist, keep in mind that you
-must pass the correct minor/major mode symbol and a string you
-want to use in the modeline *in lieu of* the original.")
+;; When you add a new element to the alist, keep in mind that you
+;; must pass the correct minor/major mode symbol and a string you
+;; want to use in the modeline *in lieu of* the original.")
 
-(defun clean-mode-line ()
-  (interactive)
-  (cl-loop for cleaner in mode-line-cleaner-alist
-        do (let* ((mode (car cleaner))
-                  (mode-str (cdr cleaner))
-                  (old-mode-str (cdr (assq mode minor-mode-alist))))
-             ;; minor mode
-             (when old-mode-str
-               (setcar old-mode-str mode-str))
-             ;; major mode
-             (when (eq mode major-mode)
-               (setq mode-name mode-str)))))
+;; (defun clean-mode-line ()
+;;   (interactive)
+;;   (cl-loop for cleaner in mode-line-cleaner-alist
+;;         do (let* ((mode (car cleaner))
+;;                   (mode-str (cdr cleaner))
+;;                   (old-mode-str (cdr (assq mode minor-mode-alist))))
+;;              ;; minor mode
+;;              (when old-mode-str
+;;                (setcar old-mode-str mode-str))
+;;              ;; major mode
+;;              (when (eq mode major-mode)
+;;                (setq mode-name mode-str)))))
 
-(add-hook 'after-change-major-mode-hook 'clean-mode-line)
+;; (add-hook 'after-change-major-mode-hook 'clean-mode-line)
+
+;; https://amitp.blogspot.com/2019/07/emacs-mode-line-simplified.html
+
+;; Mode line construct to put at the front of the mode line.
+(setq-default mode-line-front-space "")
+;; Mode line construct to report the multilingual environment.
+(setq-default mode-line-mule-info "")
+;; Mode line construct for identifying emacsclient frames.
+(setq-default mode-line-client "")
+;; Mode line construct for displaying whether current buffer is modified.
+;; (setq-default mode-line-modified '(" %1* "))
+;; Mode line construct to indicate a remote buffer.
+(setq-default mode-line-remote "")
+;; Mode line construct to describe the current frame.
+(setq-default mode-line-frame-identification "")
+;; Mode line construct to put at the end of the mode line.
+(setq-default mode-line-end-spaces "")
+;; Mode line construct for displaying major and minor modes.
+(setq-default mode-line-modes '("%[" mode-name mode-line-process "%n" "%]"))
+
+(make-face 'mode-line-read-only-face)
+(set-face-attribute 'mode-line-read-only-face nil :inherit 'mode-line
+                    :foreground nil
+                    :background "Purple")
+
+(make-face 'mode-line-read-only-face-inactive)
+(set-face-attribute 'mode-line-read-only-face-inactive nil :inherit 'mode-line-inactive
+                    :foreground nil
+                    :background "Purple")
+
+(make-face 'mode-line-modified-face)
+(set-face-attribute 'mode-line-modified-face nil :inherit 'mode-line
+                    :foreground nil
+                    :background "Red")
+
+(make-face 'mode-line-modified-face-inactive)
+(set-face-attribute 'mode-line-modified-face-inactive nil :inherit 'mode-line-inactive
+                    :foreground nil
+                    :background "Red")
+
+(defvar selected-window nil)
+
+(defun record-selected-window ()
+  (setq selected-window (selected-window)))
+
+(add-hook 'buffer-list-update-hook #'record-selected-window)
+
+(defun window-active-p ()
+  (eq selected-window (selected-window)))
+
+;; (setq-default mode-line-modified
+;;               '(:eval
+;;                 (cond (buffer-read-only
+;;                        (propertize " R " 'face
+;;                                    (if (window-active-p)
+;;                                        'mode-line-read-only-face
+;;                                      'mode-line-read-only-face-inactive)))
+;;                       ((buffer-modified-p)
+;;                        (propertize " * " 'face
+;;                                    (if (window-active-p)
+;;                                        'mode-line-modified-face
+;;                                      'mode-line-modified-face-inactive)))
+;;                       (t "   "))))
+
+(setq-default mode-line-modified
+              '(:eval
+                (cond (buffer-read-only
+                       (if (window-active-p)
+                           (propertize " [R] " 'face 'mode-line-read-only-face) " [R] "))
+                      ((buffer-modified-p)
+                       (if (window-active-p)
+                           (propertize " [+] " 'face 'mode-line-modified-face) " [+] "))
+                      (t "     "))))
+
+(defun mode-line-project-root ()
+  (or (cdr (project-current)) default-directory))
+
+(defun mode-line-buffer-name ()
+  (cond (buffer-file-name
+         (s-chop-prefix (modeline-project-root) (abbreviate-file-name buffer-file-name)))
+        (t "%b")))
+
+;; Mode line construct for identifying the buffer being displayed.
+(setq-default mode-line-buffer-identification '(:eval (mode-line-buffer-name)))
