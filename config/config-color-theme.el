dde985474bae7618ce452e6c6e546810f679cda8
lbrayner <lbrayner@users.noreply.github.com>
2021-03-26 13:14:13 -0300

    Comments removed

--- config/config-color-theme.el
+++ config/config-color-theme.el
@@ -4,24 +4,6 @@
 (load-theme 'solarized t)
 (set-face-attribute 'default nil :height 105)
 
-;; (defun solarized-init-frame (frame)
-;;   (let ((mode (if (display-graphic-p frame) 'light 'dark)))
-;;     (set-frame-parameter frame 'background-mode mode)
-;;     (set-terminal-parameter frame 'background-mode mode))
-;;   (enable-theme 'solarized))
-
-;; (add-hook 'after-make-frame-functions #'solarized-init-frame)
-
-    ;; melpa
-        ;; solarized
-;; (setq solarized-use-variable-pitch nil
-;;       solarized-scale-org-headlines nil)
-;; (setq solarized-high-contrast-mode-line t)
-;; (load-theme 'solarized-dark t)
-        ;; dracula
-; (if (window-system)
-;     (load-theme 'dracula t))
-
   ;; from solarized-theme's solarized-dark
 (let ((yellow "#b58900"))
   (if global-display-line-numbers-mode
