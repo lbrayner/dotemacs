1ad17f7ba92bd28debf3d7ba131679d91ded5253
lbrayner <lbrayner@users.noreply.github.com>
2021-04-17 12:00:06 -0300

    Reorganized code in config-org.el

--- config/config-org.el
+++ config/config-org.el
@@ -1,6 +1,46 @@
+(setq org-export-time-stamp-file nil)
+(setq org-html-validation-link nil)
+(setq-default org-export-with-author nil)
+(setq-default org-export-with-toc nil)
+(setq-default org-use-sub-superscripts '{})
+(setq-default org-export-with-sub-superscripts '{})
+
+;; babel
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (shell . t)))
+
+;; allows org-time-stamp-custom-formats to be file-local:
+
+;; https://emacs.stackexchange.com/a/2247
+(defun org-time-stamp-custom-formats-safep (value)
+  (and (consp value)
+       (stringp (car value))
+       (stringp (cdr value))))
+
+(put 'org-time-stamp-custom-formats 'safe-local-variable
+     #'org-time-stamp-custom-formats-safep)
+
+(defun org-mode-setup ()
+  "Setup Org mode."
+  (visual-line-mode t)
+  (setq truncate-lines nil)
+  (flyspell-mode)
+  (electric-indent-local-mode 0))
+
+(add-hook 'org-mode-hook #'org-mode-setup)
+
+(defun org-mode-evil-setup ()
+  "Setup Evil in Org mode."
+  (setq evil-auto-indent nil))
+
+(with-eval-after-load 'evil
+  (add-hook 'org-mode-hook #'org-mode-evil-setup))
+
 ;; https://stackoverflow.com/a/37132338/2856535
 (defun org-inline-css (exporter)
-  "Insert custom inline css"
+  "Insert custom inline css."
   (when (eq exporter 'html)
     (let* ((dir (ignore-errors (file-name-directory (buffer-file-name))))
            (filename (concat (file-name-base (buffer-file-name)) ".css"))
@@ -20,48 +60,15 @@
                               "/*]]>*/-->\n"
                               "</style>\n"))))))
 
-(defun org-mode-setup ()
-  "Setup Org mode."
-  (visual-line-mode t)
-  (setq truncate-lines nil)
-  (flyspell-mode)
-  (electric-indent-local-mode 0))
-
-(defun org-mode-evil-setup ()
-  (setq evil-auto-indent nil))
-
-(setq org-export-time-stamp-file nil)
-(setq org-html-validation-link nil)
-(setq-default org-export-with-author nil)
-(setq-default org-export-with-toc nil)
-(setq-default org-use-sub-superscripts '{})
-(setq-default org-export-with-sub-superscripts '{})
-(add-hook 'org-mode-hook #'org-mode-setup)
-(with-eval-after-load 'evil
-  (add-hook 'org-mode-hook #'org-mode-evil-setup))
 (add-hook 'org-export-before-processing-hook #'org-inline-css)
-;; babel
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((emacs-lisp . t)
-   (shell . t)))
 
-;; I'm big on org exporting
+;; Org related packages
+
+    ;; ox.el --- Export Framework for Org Mode
 (with-eval-after-load 'org
   (require 'ox))
 
-;; allows org-time-stamp-custom-formats to be file-local:
-
-;; https://emacs.stackexchange.com/a/2247
-(defun org-time-stamp-custom-formats-safep (value)
-  (and (consp value)
-       (stringp (car value))
-       (stringp (cdr value))))
-
-(put 'org-time-stamp-custom-formats 'safe-local-variable
-     #'org-time-stamp-custom-formats-safep)
-
-    ;; melpa
+    ;; MELPA
         ;; ox-reveal
-(require 'ox-reveal)
 (setq org-reveal-title-slide "<h1> %t </h1>  <br> %a <br> %e")
+(require 'ox-reveal)
